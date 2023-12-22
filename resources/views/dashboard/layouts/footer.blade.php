bbcb87529d2bd41b972ff3b414e557df27fbf3c6
kelompokuas <ferryawan.a@students.esqbs.ac.id>
2023-12-22 15:28:21 +0700

    Update laravel Hilman, Ferry

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+		<b>Version</b> 1.0.0
+	</div>
+	<strong>Sistem Pemesanan Makanan &copy; 2024 <a target="_blank">Restaurant Combucha APP</a>.</strong> Di buat di Jakarta <a href='#' title='#' target='_blank'>Kelompok Hilman, Ferryawan & Rio</a>
+	
+</footer>
